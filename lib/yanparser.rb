a02439e8db02abf154241bd7a1628cdbf1b19930
IgorPolyakov <fox.user.3@gmail.com>
2016-12-21 21:03:24 +0700

    simple parse

--- lib/yanparser.rb
+++ lib/yanparser.rb
@@ -1,3 +1,9 @@
+require 'open-uri'
+require 'nokogiri'
 class YanParser
-
+  @page
+  def title
+    @page = Nokogiri::HTML(open("https://www.yandex.ru/"))
+    @page.css('title').text
+  end
 end
--- spec/yanparser_spec.rb
+++ spec/yanparser_spec.rb
@@ -3,7 +3,6 @@ require 'yanparser'
 RSpec.describe YanParser do
   it 'Show main page' do
     qua = YanParser.new
-    # qua.set_first_point([1.0, 1.0])
-    # expect(qua.p_zero).to eq [1.0, 1.0]
+    expect(qua.title).to eq "Яндекс"
   end
 end
