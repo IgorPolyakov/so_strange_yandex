6b4ad6b954b3e2115291201838d9db3fab8cfd69
IgorPolyakov <fox.user.3@gmail.com>
2016-12-21 20:11:48 +0700

    init repo && init rspec

---
+++ lib/yanparser.rb
@@ -0,0 +1,3 @@
+class YanParser
+
+end
---
+++ spec/yanparser_spec.rb
@@ -0,0 +1,9 @@
+require 'rspec'
+require 'yanparser'
+RSpec.describe YanParser do
+  it 'Show main page' do
+    qua = YanParser.new
+    # qua.set_first_point([1.0, 1.0])
+    # expect(qua.p_zero).to eq [1.0, 1.0]
+  end
+end
